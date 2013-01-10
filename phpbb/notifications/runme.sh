b7d63e09acd74580ba9220a8e1fcd29bdbeb804d
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-10 06:56:27 -0500

    updated notifications stuff

--- phpbb/notifications/runme.sh
+++ phpbb/notifications/runme.sh
@@ -7,7 +7,7 @@ b=/home/pie/apps/test-repo/phpbb/notifications
 :>$b/p3;
 :>$b/p4;
 for c in `cat $b/clist`; do git rh; rm -rf `git st`; git cp -n $c; git reset;
-for k in 1 2 3 4; do
+for k in 1 2 3; do
 for i in `cat $b/f$k`; do if test -e $i; then git add $i; 
 else git rm -fr $i 2>/dev/null; fi; done;
 git ci -C $c && git show HEAD|head -1|awk '{print $2}' >> $b/p$k;
