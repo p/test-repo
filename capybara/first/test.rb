feab20f591266c5527b8a6f9f9fcafca8c341e57
Oleg Pudeyev <oleg@bsdpower.com>
2013-02-14 03:15:30 -0500

    Workaround until poltergeist 1.0.3 is released

--- capybara/first/test.rb
+++ capybara/first/test.rb
@@ -3,6 +3,25 @@ require 'capybara/dsl'
 require 'capybara/poltergeist'
 require 'test/unit'
 
+# fix for freebsd
+
+class Capybara::Poltergeist::WebSocketServer
+  def start_server
+    time = Time.now
+
+    begin
+      TCPServer.open('127.0.0.1', port)
+    rescue Errno::EADDRINUSE
+      if (Time.now - time) < BIND_TIMEOUT
+        sleep(0.01)
+        retry
+      else
+        raise
+      end
+    end
+  end
+end
+
 Capybara.default_driver = :poltergeist
 
 #Capybara.configure do |config|
